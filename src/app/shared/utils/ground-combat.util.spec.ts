e4e0e250d73b13378d0b5b4c27a953c7f396c6b9
David Garcia <davidgmingolo@gmail.com>
2022-02-16 23:47:00 -0600

    Finished getDodgeThreshold() function. Added some error handling to getWeaponSkill() function. Decoupled getAttackSkill() and getWeaponSkill() functions.

--- src/app/shared/utils/ground-combat.util.spec.ts
+++ src/app/shared/utils/ground-combat.util.spec.ts
@@ -3,10 +3,70 @@ import { DamageType, WeaponClass } from './ground-combat-constants';
 import { GroundCombat } from './ground-combat.util';
 
 
+describe('getDodgeThreshold()', () => {
+
+  it('should return 58 if both dodge and attackSkill are 0', () => {
+    expect(GroundCombat.getDodgeThreshold(0, 0)).toEqual(58);
+  });
+
+  it('should return 83 if Dodge 2, attackSkill 4', () => {
+    expect(GroundCombat.getDodgeThreshold(2, 4)).toEqual(83);
+  });
+
+
+  it('should return 83 if Dodge 2, attackSkill 4.5', () => {
+    expect(GroundCombat.getDodgeThreshold(2, 4.5)).toEqual(83);
+  });
+
+  it('should return 72 if Dodge 3, attackSkill 4', () => {
+    expect(GroundCombat.getDodgeThreshold(3, 4)).toEqual(72);
+  });
+
+  it('should return 92 if Dodge 2, attackSkill 5', () => {
+      expect(GroundCombat.getDodgeThreshold(2, 5)).toEqual(92);
+  });
+
+  it('should return 72 if Dodge 2, attackSkill 3.5', () => {
+      expect(GroundCombat.getDodgeThreshold(2, 3.5)).toEqual(72);
+  });
+
+  it('should return 99 if Dodge 0, attackSkill 7.5', () => {
+    expect(GroundCombat.getDodgeThreshold(0, 7.5)).toEqual(99);
+  });
+
+  it('should return 3 if Dodge 5, attackSkill 0', () => {
+    expect(GroundCombat.getDodgeThreshold(5, 0)).toEqual(3);
+  });
+
+  it('should return 83 if Dodge 5, attackSkill 7.5', () => {
+    expect(GroundCombat.getDodgeThreshold(5, 7.5)).toEqual(83);
+  });
+
+  it('should return error if Dodge 5, attackSkill 8', () => {
+    expect(() => GroundCombat.getDodgeThreshold(5, 8)).toThrow(new Error("Invalid attack skill value."));
+  });
+
+  it('should return error if Dodge 5, attackSkill -1', () => {
+    expect(() => GroundCombat.getDodgeThreshold(5, -1)).toThrow(new Error("Invalid attack skill value."));
+  });
+
+  it('should return error if Dodge 6, attackSkill 7.5', () => {
+    expect(() => GroundCombat.getDodgeThreshold(6, 7.5)).toThrow(new Error("Invalid dodge skill value."));
+  });
+
+  it('should return error if Dodge -1, attackSkill 7.5', () => {
+    expect(() => GroundCombat.getDodgeThreshold(-1, 7.5)).toThrow(new Error("Invalid dodge skill value."));
+  });
+});
+
 describe('getRangeModifier()', () => {
 
-  it('should return 0.525 if all inputs are 0', () => {
-    expect(GroundCombat.getRangeModifier(0, 0, 0)).toEqual(0.525);
+  it('should return 1 if all inputs are 0', () => {
+    expect(GroundCombat.getRangeModifier(0, 0, 0)).toEqual(1);
+  });
+
+  it('should return 1 if optimumRange and currentRange are the same', () => {
+    expect(GroundCombat.getRangeModifier(6, 6, 4.5)).toEqual(1);
   });
 
   it('should return ~0.1602 if optimumRange is 1, currentRange is 0, and weaponDropOff is 0', () => {
@@ -37,39 +97,35 @@ describe('getRangeModifier()', () => {
 describe('getAttackSkill()', () => {
 
     it('should return 0 if all skills are 0', () => {
-        expect(GroundCombat.getAttackSkill(0, 0, 0, 0, 0, false, WeaponClass.Projectile, DamageType.EnergyP)).toEqual(0);
+        expect(GroundCombat.getAttackSkill(0, 0)).toEqual(0);
     });
 
-    it('should return 4 if Dex 3, PW 2, NPW 4, HW 1, and attacking with projectile', () => {
-        expect(GroundCombat.getAttackSkill(3, 2, 4, 1, 0, false, WeaponClass.Projectile, DamageType.EnergyP)).toEqual(4);
+    it('should return 4 if Dex 3, weaponSkill 2', () => {
+        expect(GroundCombat.getAttackSkill(3, 2)).toEqual(4);
     });
 
-    it('should return 5 if Dex 3, PW 2, NPW 4, HW 1, and attacking with non-projectile', () => {
-        expect(GroundCombat.getAttackSkill(3, 2, 4, 1, 0, false, WeaponClass.NonProjectile, DamageType.PhysicalP)).toEqual(5);
+    it('should return 5 if Dex 3, weaponSkill 4', () => {
+        expect(GroundCombat.getAttackSkill(3, 4)).toEqual(5);
     });
 
-    it('should return 3.5 if Dex 3, PW 2, NPW 4, HW 1, and attacking with heavy projectile', () => {
-        expect(GroundCombat.getAttackSkill(3, 2, 4, 1, 0, false, WeaponClass.HeavyProjectile, DamageType.EnergyH)).toEqual(3.5);
+    it('should return 3.5 if Dex 3, weaponSkill 1', () => {
+        expect(GroundCombat.getAttackSkill(3, 1)).toEqual(3.5);
     });
 
-    it('should return 4 if Dex 3, PW 2, NPW 4, HW 1, not a forcie, and attacking with lightsaber', () => {
-        expect(GroundCombat.getAttackSkill(3, 2, 4, 1, 5, false, WeaponClass.NonProjectile, DamageType.Lightsaber)).toEqual(4);
-    });
-
-    it('should return 4.5 if Dex 3, PW 0, NPW 2, HW 1, forcie with LS 5, and attacking with lightsaber', () => {
-        expect(GroundCombat.getAttackSkill(3, 0, 2, 1, 5, true, WeaponClass.NonProjectile, DamageType.Lightsaber)).toEqual(4.5);
+    it('should return 4.5 if Dex 3, weaponSkill 3', () => {
+        expect(GroundCombat.getAttackSkill(3, 3)).toEqual(4.5);
     });
 
     it('should return 7.5 if the final result is greater than 7.5', () => {
-        expect(GroundCombat.getAttackSkill(30, 0, 2, 1, 5, true, WeaponClass.NonProjectile, DamageType.Lightsaber)).toEqual(7.5);
+        expect(GroundCombat.getAttackSkill(30, 5)).toEqual(7.5);
     });
 
     it('should return 0 if the final result is less than 0', () => {
-        expect(GroundCombat.getAttackSkill(-30, 0, 2, 1, 5, true, WeaponClass.NonProjectile, DamageType.Lightsaber)).toEqual(0);
+        expect(GroundCombat.getAttackSkill(-30, 0)).toEqual(0);
     });
 
-    it('should return 7.5 if Dex 5, PW 2, NPW 5, HW 1, and attacking with non-projectile', () => {
-      expect(GroundCombat.getAttackSkill(5, 2, 5, 1, 0, false, WeaponClass.NonProjectile, DamageType.PhysicalP)).toEqual(7.5);
+    it('should return 7.5 if Dex 5, weaponSkill 5', () => {
+      expect(GroundCombat.getAttackSkill(5, 5)).toEqual(7.5);
     });
 });
 
@@ -109,5 +165,21 @@ describe('getWeaponSkill()', () => {
 
     it('should return 2 if PW 2, NPW 4, HW 1, not a forcie with LS, and attacking with lightsaber', () => {
       expect(GroundCombat.getWeaponSkill(2, 4, 1, 5, false, WeaponClass.NonProjectile, DamageType.Lightsaber)).toEqual(2);
-  });
+    });
+
+    it('should return error if Lightsaber skill is 6', () => {
+      expect(() => GroundCombat.getWeaponSkill(2, 4, 1, 6, false, WeaponClass.NonProjectile, DamageType.Lightsaber)).toThrow(new Error("Invalid lightsaber skill value."));
+    });
+
+    it('should return error if Lightsaber skill is -1', () => {
+      expect(() => GroundCombat.getWeaponSkill(2, 4, 1, -1, false, WeaponClass.NonProjectile, DamageType.Lightsaber)).toThrow(new Error("Invalid lightsaber skill value."));
+    });
+
+    it('should return error if NPW skill is 6', () => {
+      expect(() => GroundCombat.getWeaponSkill(2, 6, 1, 5, false, WeaponClass.NonProjectile, DamageType.Lightsaber)).toThrow(new Error("Invalid npwSkill skill value."));
+    });
+
+    it('should return error if NPW skill is -1', () => {
+      expect(() => GroundCombat.getWeaponSkill(2, -1, 1, 5, false, WeaponClass.NonProjectile, DamageType.Lightsaber)).toThrow(new Error("Invalid npwSkill skill value."));
+    });
 });
--- src/app/shared/utils/ground-combat.util.ts
+++ src/app/shared/utils/ground-combat.util.ts
@@ -1,12 +1,21 @@
-import { DamageType, lsWeaponSkill, WeaponClass } from "./ground-combat-constants";
+import { DamageType, dodgeThreshold, lsWeaponSkill, WeaponClass } from "./ground-combat-constants";
 
 export class GroundCombat {
-    public static getDodgeThreshold() {
+    public static getDodgeThreshold(defendDodge: number, attackSkill: number) {
 
+      if (defendDodge < 0 || defendDodge > 5)
+        throw new Error("Invalid dodge skill value.");
+      if (attackSkill < 0 || attackSkill > 7.5)
+        throw new Error("Invalid attack skill value.");
+
+      return dodgeThreshold[defendDodge][Math.floor(attackSkill)];
     }
 
     public static getRangeModifier(optimumRange: number, currentRange: number, weaponDropOff: number) {
       const deltaRange = Math.abs(optimumRange - currentRange);
+      if (deltaRange === 0)
+        return 1;
+
       let rangeMod = (1 - (0.95 / (1 + Math.E**(-2 * (deltaRange - weaponDropOff))))) - (0.003 * deltaRange);
       rangeMod = rangeMod > 0 ? rangeMod : 0;
 
@@ -14,11 +23,8 @@ export class GroundCombat {
     }
 
     public static getAttackSkill(
-      dex: number, pwSkill: number, npwSkill: number,
-      hwSkill: number, lightSkill: number, force: boolean,
-      weaponClass: WeaponClass, damageType: DamageType)  {
+      dex: number, weaponSkill: number)  {
 
-      let weaponSkill = this.getWeaponSkill(pwSkill, npwSkill, hwSkill, lightSkill, force, weaponClass, damageType);
       let attackSkill = dex + weaponSkill/2;
       attackSkill = attackSkill > 7.5 ? 7.5 : attackSkill;
       attackSkill = attackSkill < 0 ? 0 : attackSkill;
@@ -30,6 +36,11 @@ export class GroundCombat {
       pwSkill: number, npwSkill: number, hwSkill: number,
       lightSkill: number, force: boolean, weaponClass: WeaponClass,
       damageType: DamageType) {
+
+        if (npwSkill < 0 || npwSkill > 5)
+          throw new Error("Invalid npwSkill skill value.");
+        if (lightSkill < 0 || lightSkill > 5)
+          throw new Error("Invalid lightsaber skill value.");
 
         let weaponSkill = pwSkill;
         if (damageType === DamageType.Lightsaber) {
